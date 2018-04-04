9cb405bca99c94b1dc7f3babf3ecfb9da239118c
Børset Tor Røkkum <TOBORS@vegvesen.no>
2018-04-04 12:13:50 +0200

    Forms

---
+++ Forms/src/components/Skredrose.vue
@@ -0,0 +1,237 @@
+<template lang="html">
+  <div class="container" >
+    <div class="row">
+      <svg >
+        <defs
+           id="defs4646">
+          <linearGradient
+             id="linearGradient4652"
+             osb:paint="solid">
+            <stop
+               style="stop-color:#000000;stop-opacity:1;"
+               offset="0"
+               id="stop4650" />
+          </linearGradient>
+          <linearGradient
+             inkscape:collect="always"
+             xlink:href="#linearGradient4652"
+             id="linearGradient4654"
+             x1="37.3"
+             y1="9"
+             x2="52.7"
+             y2="9"
+             gradientUnits="userSpaceOnUse" />
+        </defs>
+        <sodipodi:namedview
+           pagecolor="#ffffff"
+           bordercolor="#666666"
+           borderopacity="1"
+           objecttolerance="10"
+           gridtolerance="10"
+           guidetolerance="10"
+           inkscape:pageopacity="0"
+           inkscape:pageshadow="2"
+           inkscape:window-width="1846"
+           inkscape:window-height="1053"
+           id="namedview4644"
+           showgrid="false"
+           inkscape:zoom="3.0405592"
+           inkscape:cx="-13.692456"
+           inkscape:cy="55.220343"
+           inkscape:window-x="65"
+           inkscape:window-y="-9"
+           inkscape:window-maximized="1"
+           inkscape:current-layer="g4640" />
+        <circle
+           cx="50.262192"
+           cy="50.262192"
+           class="circle-outline"
+           id="circle4600"
+           style="display:inline;fill:#d6d6d6;fill-opacity:1;stroke:#000000;stroke-width:1.12041628;stroke-opacity:1"
+           r="40.334984"
+           inkscape:label="Bakgrunn" />
+        <g
+           id="g4640"
+           transform="matrix(1.1204162,0,0,1.1204162,-0.15654114,-0.15654114)"
+           style="display:inline"
+           inkscape:label="Skredrose">
+          <g  @click="test" @click.once="tomSkjema"
+             id="kaker"
+             style="display:inline;fill:lightgray"
+             inkscape:label="Kakestykke">
+            <path
+               id="N"
+               class="sector active"
+               transform="rotate(-22.5,45,45)"
+               d="M 45,9 A 36,36 0 0 1 70.455844,19.544156 L 45,45 Z"
+              />
+            <path
+               id="NØ"
+               class="sector active"
+               transform="rotate(22.5,45,45)"
+               d="M 45,9 A 36,36 0 0 1 70.455844,19.544156 L 45,45 Z" />
+            <path
+               id="Ø"
+               class="sector active"
+               transform="rotate(67.5,45,45)"
+               d="M 45,9 A 36,36 0 0 1 70.455844,19.544156 L 45,45 Z" />
+            <path
+               id="SØ"
+               class="sector active"
+               transform="rotate(112.5,45,45)"
+               d="M 45,9 A 36,36 0 0 1 70.455844,19.544156 L 45,45 Z" />
+            <path
+               id="S"
+               class="sector active"
+               transform="rotate(157.5,45,45)"
+               d="M 45,9 A 36,36 0 0 1 70.455844,19.544156 L 45,45 Z" />
+            <path
+               id="SV"
+               class="sector active"
+               transform="rotate(-157.5,45,45)"
+               d="M 45,9 A 36,36 0 0 1 70.455844,19.544156 L 45,45 Z" />
+            <path
+               id="V"
+               class="sector active"
+               transform="rotate(-112.5,45,45)"
+               d="M 45,9 A 36,36 0 0 1 70.455844,19.544156 L 45,45 Z" />
+            <path
+               id="NV"
+               class="sector active"
+               transform="rotate(-67.5,45,45)"
+               d="M 45,9 A 36,36 0 0 1 70.455844,19.544156 L 45,45 Z" />
+          </g>
+          <g
+             id="g4608"
+             inkscape:label="N">
+            <circle
+               cx="45"
+               cy="9"
+               r="7.1999998"
+               class="circle-label"
+               id="circle4604"
+               style="fill:#ffffff;fill-opacity:1;stroke:url(#linearGradient4654);stroke-opacity:1" />
+            <text
+               x="42.697792"
+               y="8.671113"
+               dy="3.168"
+               font-size="7.920000000000001"
+               class="centered"
+               id="text4606"
+               style="font-size:7.92000008px;fill:#000000;fill-opacity:1;stroke:#171717;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1">N</text>
+          </g>
+          <g
+             id="g4614"
+             style="fill:#ffffff;fill-opacity:1;stroke:#0f0000;stroke-opacity:1"
+             inkscape:label="Ø">
+            <circle
+               cx="81"
+               cy="45"
+               r="7.1999998"
+               class="circle-label"
+               id="circle4610"
+               style="fill:#ffffff;fill-opacity:1;stroke:#0f0000;stroke-opacity:1" />
+            <text
+               x="78.368904"
+               y="45"
+               dy="3.168"
+               font-size="7.920000000000001"
+               class="centered"
+               id="text4612"
+               style="font-size:7.92000008px;fill:#000000;fill-opacity:1;stroke:#171717;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1">Ø</text>
+          </g>
+          <g
+             id="g4620"
+             style="fill:#ffffff;fill-opacity:1;stroke:#0f0000;stroke-opacity:1"
+             inkscape:label="S">
+            <circle
+               cx="45"
+               cy="81"
+               r="7.1999998"
+               class="circle-label"
+               id="circle4616"
+               style="fill:#ffffff;fill-opacity:1;stroke:#0f0000;stroke-opacity:1" />
+            <text
+               x="42.368904"
+               y="80.342224"
+               dy="3.168"
+               font-size="7.920000000000001"
+               class="centered"
+               id="text4618"
+               style="font-size:7.92000008px;fill:#000000;fill-opacity:1;stroke:#171717;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1">S</text>
+          </g>
+          <g
+             id="g4626"
+             style="fill:#ffffff;fill-opacity:1;stroke:#0f0000;stroke-opacity:1"
+             inkscape:label="V">
+            <circle
+               cx="9"
+               cy="45"
+               r="7.1999998"
+               class="circle-label"
+               id="circle4622"
+               style="fill:#ffffff;fill-opacity:1;stroke:#400000;stroke-opacity:1" />
+            <text
+               x="6.3689051"
+               y="45"
+               dy="3.168"
+               font-size="7.920000000000001"
+               class="centered"
+               id="text4624"
+               style="font-size:7.92000008px;fill:#000000;fill-opacity:1;stroke:#171717;stroke-width:0;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1">V</text>
+          </g>
+        </g>
+      </svg>
+    </div>
+    <div class="row">
+      <button type="button" class="btn btn-primary" name="button" @click="tomSkjema"> Tøm skjema</button>
+    </div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+
+    }
+  },
+  methods: {
+    test(event) {
+      console.log(event);
+      let test = document.querySelectorAll('#kaker path')
+      event.target.classList.toggle('active')
+      console.log(Array.prototype.map.call(test, function(obj) {
+        if (obj.classList.contains('active')) {
+          return obj.id;
+        }
+  }));
+      // console.log(test);
+    },
+    tomSkjema(event) {
+      let test = document.querySelectorAll('#kaker path')
+      Array.prototype.map.call(test, function(obj) {
+        obj.classList.remove('active')
+      })
+      event.target.classList.toggle('active')
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  #kaker path{
+    cursor: pointer;
+    stroke: black;
+    stroke-width: 1px;
+    transition: fill 0.3s
+    /* fill:white */
+  }
+
+  .active, #kaker path:hover {
+    fill: grey;
+  }
+</style>

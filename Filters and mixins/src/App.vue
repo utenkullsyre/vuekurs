7da49ad643ddd8590fa9068db72dd3c0d8dab090
Børset Tor Røkkum <TOBORS@vegvesen.no>
2018-04-10 10:13:26 +0200

    Vue copytext

--- Filters and mixins/src/App.vue
+++ Filters and mixins/src/App.vue
@@ -11,10 +11,16 @@
                 </ul>
             </div>
         </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+              <app-fruit v-for="fruit in fruits" :fruit="fruit"></app-fruit>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import Fruit from './components/Fruit.vue'
     export default {
       data() {
         return {
@@ -34,6 +40,9 @@
             return element.match(this.filterText);
           })
         }
+      },
+      components: {
+        appFruit: Fruit
       }
     }
 </script>
---
+++ Filters and mixins/src/components/Fruit.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="row">
+    <p> {{ fruit }}</p>
+    <button class="btn btn-primary" v-clipboard:copy="fruit">Kopier dette</button>
+    <hr>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['fruit']
+}
+</script>
+
+<style lang="css">
+</style>
